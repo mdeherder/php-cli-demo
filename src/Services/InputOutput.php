cb07526a580352b21c0dbdafc1f063126792aee0
Deherder Michel <webmaster@cyel.be>
2022-02-15 21:54:38 +0100

    font-bold

--- src/Services/InputOutput.php
+++ src/Services/InputOutput.php
@@ -18,25 +18,25 @@ class InputOutput extends SymfonyStyle
     // Display a message in case of right answer.
     public function annonce(string $message): void
     {
-        render('<div class="px-2">&nbsp;<div class="p-1 bg-yellow-600">'.$message.'</div></div>');
+        render('<div class="px-2">&nbsp;<div class="p-1 bg-yellow-600 text-black">'.$message.'</div></div>');
     }
 
     // Display a message in case of right answer.
     public function right(string $message): void
     {
-        render('<div class="px-2"><div class="p-1 bg-green-900">'.$message.'</div></div>');
+        render('<div class="px-2"><div class="p-1 bg-green-600 font-bold">'.$message.'</div></div>');
     }
 
     // Display a message in case of wrong answer.
     public function wrong(string $message): void
     {
-        render('<div class="px-2"><div class="p-1 bg-red-600">'.$message.'</div></div>');
+        render('<div class="px-2"><div class="p-1 bg-red-600 font-bold">'.$message.'</div></div>');
     }
 
     // Display a info message.
     public function result(string $message): void
     {
-        render('<div class="px-2">&nbsp;<div class="p-1 bg-blue-600">'.$message.'</div></div><div>&nbsp;</div>');
+        render('<div class="px-2">&nbsp;<div class="p-1 bg-blue-600 font-bold">'.$message.'</div></div><div>&nbsp;</div>');
         render('<span>&nbsp;</span>');
         //     $this->block($message, null, 'fg=white;bg=blue', ' ', true);
     }
